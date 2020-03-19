b1c40b524757ad6562842febd61d2bf0574c0006
Javier Esteban Torres <61968050+JaviEstebanT@users.noreply.github.com>
2020-03-19 18:46:55 +0100

    Add files via upload

---
+++ java/printTime.js
@@ -0,0 +1,38 @@
+// JavaScript source code
+function printTime() {
+  var d = new Date();
+  var diaSem = d.getDay();
+    switch (diaSem) {
+     case 1:
+        diaSem = "Lunes";
+        break;
+     case 2:
+        diaSem = "Martes";
+        break;
+     case 3:
+        diaSem = "Miercoles";
+        break;
+     case 4:
+        diaSem = "Jueves";
+        break;
+     case 5:
+        diaSem = "Viernes";
+        break;
+     case 6:
+        diaSem = "Sabado";
+        break;
+     default:
+        diaSem = "Domingo";
+    }
+  var dia = d.getDate();
+  var mes = d.getMonth();
+  var ano = d.getFullYear();
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  var hora = document.getElementById("hora");
+  hora.style.fontSize="medium";
+  hora.style.color="black"
+  hora.innerHTML = diaSem+", "+dia+"/"+mes+"/"+ano+" "+hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
